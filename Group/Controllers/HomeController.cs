32895bf38d9009bbf3ca90840dfeccfb1519e0bf
brittaneyross <brittaney.e.ross@gmail.com>
2018-04-21 19:27:52 -0500

    Added occupation model to store occupation details. Updated results action to display profession name, description, probability and related jobs. Currently a string is being used as a place holder in the results action

---
+++ Group/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Group.Models;
+using System.Data.Entity;
+
+
+namespace Group.Controllers
+{
+    public class HomeController : Controller
+    {   
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About This Project";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact SCPS";
+
+            return View();
+        }
+
+        public ActionResult Result(string OccupationQuery)
+        {   
+            //replace with OccupationQuery or aurgument passed from index
+            string placeholder = "Clergy";
+
+            ViewBag.Message = "Results Page";
+
+            AutomationEntities_ j = new AutomationEntities_();
+            var jobs = j.sp_jobstable().ToList();
+            ViewBag.jobsInTable = jobs;
+
+            var jobsRelated = j.sp_Related(placeholder).ToList();
+            ViewBag.jobsRelatedTable = jobsRelated;
+
+            var descrip = j.occDescription(placeholder).FirstOrDefault();
+            var prob = j.occProbability(placeholder).FirstOrDefault();
+
+            Occupation occ = new Occupation()
+            {
+                occName = placeholder,
+                occDescription = descrip,
+                occProbability = prob
+            };
+
+            ViewData["OccupationData"] = occ;
+
+            return View(occ);
+        }
+
+        public ActionResult Sorry()
+        {
+            return View();
+        }
+    }
+}
---
+++ Group/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Group.MvcApplication" Language="C#" %>
