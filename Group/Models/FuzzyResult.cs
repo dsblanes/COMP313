df8ee70da72c4cf181a16d08448d644aca38d2af
rconnolly86 <33334040+rconnolly86@users.noreply.github.com>
2018-04-22 12:21:11 -0500

    Search Update
    Re-implemented Linda's autocomplete.js into the MVC model. Created new ActionResult that takes the search input on submit and passes that as a paremeter into Result() ActionResult.
    
    Still needs to be implemented on the Result() page to be able to search again.

---
+++ Group/Models/FuzzyResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Group.Models
+{
+    public class FuzzyResult
+    {
+
+        public string fresult { get; set; }
+
+    }
+
+}
